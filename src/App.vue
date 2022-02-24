eff0622d3f83aedc4339dd1ffbcaed0d0b2f651d
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2022-02-24 15:57:49 +0100

    first push

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,277 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div >
+        <header>
+            <h1>PokéList</h1>
+            <nav>
+                <a>Home</a>
+                <a>Link1</a>
+                <a>Link2</a>
+                <a>Link3</a>
+            </nav>
+        </header>    
+    
+        <HelloWorld />
+    
+        <footer>
+            <div>
+                <h3>Data from pokeapi</h3>
+                <a>https://pokeapi.co/</a>
+            </div>
+            <img id="logopolytech" src="./assets/logopolytech.png" alt="logo de Polytech">
+            <p id="copyright">&copy; ITALIANO Lorenzo</p>
+        </footer>
+    </div>  
 </template>
-
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+
+
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  methods:{
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+        @import url('https://fonts.googleapis.com/css2?family=Anton&family=Oswald:wght@200;400;700&display=swap');  
+        /* RESET */
+html{
+    scroll-behavior: smooth;
 }
-</style>
+
+* {  
+	vertical-align: baseline;  
+	font-family: "Oswald";  
+	font-style: inherit;  
+	font-size: 100%;  
+	padding: 0;  
+	margin: 0;  
+	}   
+table {  
+	border-collapse: collapse;  
+	border-spacing: 0;  
+}
+
+ul{
+    list-style-type: none;
+}
+/* Begin */
+body{
+    display: grid;
+    max-width: 100vw;
+    min-height: 100vh;
+    /*background-image: linear-gradient(to right, #212120, #20201f, #1e1e1d, #1d1d1c, #1c1c1b, #1c1c1b, #1c1c1b, #1c1c1b, #1d1d1c, #1e1e1d, #20201f, #212120);
+  */
+    background-color:#f2f2f2;
+}
+
+header{
+    position:sticky;
+    top: 0;
+    height: 10vh;
+    background-image: linear-gradient(to right, #d1e153, #dcd341, #e6c332, #efb428, #f6a325, #ff9150, #ff8674, #ff8394, #f397cb, #d6b2f0, #b9caff, #b0dcfc);
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 0 10vw;
+    z-index:3;
+}
+
+header > h1{
+    font-size: 2.5em;
+}
+
+header > nav{
+    display: flex;
+    gap: 1em ;
+    position: absolute;
+    right: 10vw;
+    font-size: 2em;
+}
+
+header > nav > *:hover{
+    cursor:pointer;
+    opacity: 0.5;
+}
+
+main{
+    min-height: 70vh;
+    margin: 5vh 15vw;
+}
+
+footer{
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    gap:5vh 5vw;
+    height: 20vh;
+    background-image: linear-gradient(to right, #d1e153, #dcd341, #e6c332, #efb428, #f6a325, #ff9150, #ff8674, #ff8394, #f397cb, #d6b2f0, #b9caff, #b0dcfc);
+}
+
+h1{
+    font-family: "Anton";
+}
+
+button {
+    border-radius: 100rem;
+    padding: 1rem;
+    font-size: 1rem;
+    padding: .5rem 3rem;
+    color: black;
+    box-shadow: 0 0 6px 0 rgba(157, 96, 212, 0.5);
+    border: solid 3px transparent;
+    background-image: linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)), linear-gradient(to right, #d1e153, #dcd341, #e6c332, #efb428, #f6a325, #ff904c, #ff816e, #ff7b8c, #f48ac3, #d2a2ed, #a6baff, #85ccff);
+    
+    /*background-image: #f2f2f2,linear-gradient(to right, #d1e153, #dcd341, #e6c332, #efb428, #f6a325, #ff904c, #ff816e, #ff7b8c, #f48ac3, #d2a2ed, #a6baff, #85ccff);
+*/
+    background-origin: border-box;
+    background-clip: content-box, border-box;
+    box-shadow: 2px 1000px 1px #f2f2f2 inset;
+  }
+  
+button:hover {
+    box-shadow: none;
+    cursor:pointer;
+}
+
+input.searchBar{
+    border-radius: 100rem;
+    padding: 1rem;
+    font-size: 1rem;
+    padding: .5rem 3rem;
+    box-shadow: 0 0 6px 0 rgba(157, 96, 212, 0.5);
+    border: solid 3px transparent;
+    background-image: linear-gradient(rgba(255, 255, 255, 0), rgba(255, 255, 255, 0)), linear-gradient(to right, #d1e153, #dcd341, #e6c332, #efb428, #f6a325, #ff904c, #ff816e, #ff7b8c, #f48ac3, #d2a2ed, #a6baff, #85ccff);
+    background-origin: border-box;
+    background-clip: content-box, border-box;
+    box-shadow: 2px 1000px 1px #f2f2f2 inset;
+}
+
+section{
+    display: flex;
+    flex-direction: column;
+    gap: 10vh 0;
+}
+
+section > form{
+    display: flex;
+    justify-content: center;
+    gap:1vw;
+}
+section > form > input{
+    width: 70%;
+}
+
+#pokemonListul{
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+}
+
+#pokemonListul > li{
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    justify-content:center;
+    height: 35vh;
+    border-radius: 10%;
+    margin: 1vh 1vw;
+    border:solid black 2px;
+}
+
+#pokemonListul > li > div{
+    display:flex;
+    gap:1vw;
+}
+
+#pokemonListul > li > div > p {
+    border:2px solid black;
+    padding:0.5vh 0.5vw;
+    border-radius: 25%;
+}
+
+#pokemonListul > li h1{
+  font-family:"Oswald";
+  font-weight:400;
+}
+
+#pokemonListul > li:hover{
+    cursor:pointer;
+    border: 0;
+    transform:scale(1.03);
+    transition: 0.3s;
+    transition-timing-function: ease-in-out;
+    position: relative;
+}
+
+/* Small devices (landscape phones, 576px and up)*/
+@media (min-width: 200px) {
+    #logopolytech{
+        display:none;
+    }
+    main{
+        margin: 10px 10%;
+    }
+    header > nav{
+        display:none;
+    }
+    #pokemonListul{
+        grid-template-columns: 1fr;
+    }
+}
+/* Small devices (landscape phones, 576px and up)*/
+@media (min-width: 567px) {
+    #logopolytech{
+        display:none;
+    }
+    main{
+        margin: 2vh 30px;
+    }
+    header > nav{
+        display:none;
+    }
+    #pokemonListul{
+        grid-template-columns: 1fr 1fr;
+    }
+ }
+
+/* Medium devices (tablets, 768px and up)*/
+@media (min-width: 768px) {
+    #logopolytech{
+        display:block;
+    }
+    main{
+        margin: 2vh 10vw;
+    }
+    header > nav{
+        display:flex;
+    }
+    #pokemonListul{
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+}
+
+/* Extra large devices (large laptops and desktops, 1200px and up) */
+@media only screen and (min-width: 1200px) {
+    #logopolytech{
+        display:block;
+    }
+    main{
+        margin: 5vh 15vw;
+    }
+    header > nav{
+        display:flex;
+    }
+    #pokemonListul{
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+    
+ }
+ 
+ 
+</style> 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,306 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <main>
+    <section>
+      <div v-if="displayInfos" id="infosView">
+        <figure :style="{backgroundColor:colorPalette(this.pokemonToDisplay.color)}">
+          <img :src="pokemonInfoSrc" alt="Big picture of concerned pokemon">
+        </figure>
+        <div id="infosRightSide">
+          <div id="xcross">
+            <img src="../assets/xcross.png" alt="information view exit cross" @click="this.displayInfos=false">
+          </div>
+          <div id="infosNavBar">
+            <a @click.prevent="this.infoGeneral=true">Général</a>
+            <a @click.prevent="this.infoGeneral=false">Stats</a>
+          </div>
+          <div id="infosGeneral" v-show="infoGeneral">
+            <p>{{capitalizeFirstLetter(this.pokemonToDisplay.name)}}</p>
+            <p>#{{ getThreeLetterId(this.pokemonToDisplay.id)}}</p>
+            <div id="weightHeightInfo">
+              <p>Weight:{{this.pokemonToDisplay.weight}}</p>
+              <p>Height:{{this.pokemonToDisplay.height}}</p>
+            </div>
+            <div id="displayTypesDetails">
+              <p v-for="type in this.pokemonToDisplay.types" :key="type.type.name" class="types">{{type.type.name}}</p>
+            </div>
+          </div>
+          <div id="infosStats" v-show="!infoGeneral">
+            <p v-for="stat in this.pokemonToDisplay.stats" :key="stat.stat.name">{{stat.stat.name}}: {{stat.base_stat}}</p>
+          </div>
+        </div>
+      </div>
+
+      <form @submit.prevent="fetchPokemonBySearch">
+        <input type="text" class="searchBar" v-model="search" placeholder="Entrez le nom ou l'id d'un pokemon">
+        <button type="submit">Search</button>
+      </form>
+      <div v-if="this.loadedImage!=this.imageMaxCount" class="loadingScreen">
+        <img src="../assets/loading_gif.gif" alt="loading animation">
+      </div>
+      <div v-if="!loading">
+        <ul id="pokemonListul" v-show="this.loadedImage>=this.imageMaxCount">
+          <li v-for="pokemon in data.sort(GetSortOrder('id'))" v-bind:key="pokemon.id" :style="{backgroundColor:colorPalette(pokemon.color)}" @click="getPokemonInfos(pokemon)">
+            <img :src="pokemon.src" :alt="pokemon.name" @load="this.loadedImage++">
+            <h1>{{ capitalizeFirstLetter(pokemon.name) }}</h1>
+            <h5>#{{ getThreeLetterId(pokemon.id) }}</h5>
+            <div>
+              <p v-for="type in pokemon.types" :key="type.type.name">{{type.type.name}}</p>
+            </div>
+          </li>
+        </ul>
+        <ol v-if="this.loadedImage==this.imageMaxCount" class="pageList">
+          <li v-for="index in 22" :key="index" :style="{backgroundColor:isPageSelected(index)}" @click="fetchPokemonByPage(index);scrollToTop();">{{ index }}</li>
+        </ol>
+      </div>
+    </section>
+  </main>
+  
 </template>
 
 <script>
+import {ref, onMounted} from "vue"
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+    methods: {
+      GetSortOrder(prop) {    
+        return function(a, b) {    
+          if (a[prop] > b[prop]) {    
+            return 1
+          } else if (a[prop] < b[prop]) {    
+            return -1 
+        }    
+        return 0   
+      }
+    },
+    capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1)
+    },
+    getThreeLetterId(id){
+      switch(id.toString().length){
+        case 1:
+          return "00"+id
+        case 2:
+          return "0"+id
+        default:
+          return id 
+      }
+    },      
+    scrollToTop(){
+      document.documentElement.scrollTop = 0
+      console.log("scrolling to the top !")
+    },
+    fetchPokemonByPage(page){
+      this.loading = true
+      this.currentPage = page
+      this.loadedImage = 0
+      let img = 0
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon/?limit=52&offset="+(page-1)*52)
+        .then((response) => {
+          while(this.data.length > 0){
+            this.data.pop()
+          }
+          response.data.results.forEach(el => {
+            img += 1
+            let a = {}
+            axios
+              .get(el.url)
+              .then((response) => {
+                a = response.data
+                //a.src = response.data.sprites.front_default
+                a.src = this.getThreeLetterId(response.data.id) > 898 ? response.data.sprites.front_default : "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/" + this.getThreeLetterId(response.data.id)  +".png" 
+                axios
+                  .get(response.data.species.url)
+                  .then((resp) => {
+                    a.color = resp.data.color.name
+                    this.data.push(a)
+                  })
+              })
+          })
+          this.imageMaxCount = img
+          // A la main parce qu'11 images ne se chargent pas sur cette page
+          if(this.currentPage===21){
+            this.imageMaxCount -= 11
+          }else if(this.currentPage===20){
+            this.imageMaxCount -= 2
+          }
+        })
+        .then(() => {
+          console.log(this.data)
+          this.loading=false
+        })
+
+        .catch(err =>{
+          console.log(err)
+        })
+      },
+      fetchPokemonBySearch(){
+        this.loading = true
+        console.log(this.search)
+        while(this.data.length > 0){
+              this.data.pop()
+        }
+        if(this.search == ""){
+          this.fetchPokemonByPage(this.page)
+        }else{
+
+
+        this.loadedImage = 0
+        this.imageMaxCount = 1
+
+        axios
+          .get("https://pokeapi.co/api/v2/pokemon/"+this.search)
+          .then((response) => {
+            console.log(response.data)
+            let a = {}
+            a = response.data
+            //a.src = response.data.sprites.front_default
+            a.src = this.getThreeLetterId(response.data.id) > 898 ? response.data.sprites.front_default : "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/" + this.getThreeLetterId(response.data.id)  +".png" 
+            axios
+              .get(response.data.species.url)
+              .then((resp) => {
+                a.color = resp.data.color.name
+                this.data.push(a)
+              })
+            })
+          .then(() => {
+            console.log(this.data)
+            this.loading=false
+        })
+
+        .catch(err =>{
+          console.log(err)
+        })
+        }
+      },
+    getPokemonInfos(pokemon){
+      this.pokemonToDisplay=pokemon
+      console.log(pokemon.name)
+      console.log(this.pokemonToDisplay)
+      this.displayInfos = true
+    },
+    colorPalette : function(color){
+        switch(color){
+          case "blue":
+            return "#adfafc"
+          case "red":
+            return "#fcadad"
+          case "green":
+            return "#defcad"
+          case "yellow":
+            return "#fafcad"
+          case "brown":
+            return "#c4a1a0"
+          case "purple":
+            return "#d8adfc"
+          case "gray":
+            return "#c4c4c4"
+          case "pink":
+            return "#fcadef"
+          case "black":
+            return "#4c4c4c"
+          default:
+            return color
+        }
+      },
+      isPageSelected(index){
+        if(index===this.currentPage){
+          return "#ff6763"
+        }
+        else{
+          return ""
+        }
+      }
+      
+  },
+  setup(){
+    const data = ref(null)
+    const loading = ref(true)
+    const error = ref(null)
+
+
+    function getThreeLetterId(id){
+      switch(id.toString().length){
+        case 1:
+          return "00"+id
+        case 2:
+          return "0"+id
+        default:
+          return id 
+      }
+    }
+
+    function fetchData(){
+      loading.value = true
+      axios
+        .get("https://pokeapi.co/api/v2/pokemon/?limit=52")
+        .then(function(response) {
+          console.log(response.data)
+          data.value = []
+          response.data.results.forEach(el => {
+            let a = {}
+            axios
+              .get(el.url)
+              .then(function(response){
+                a = response.data
+                //a.src = response.data.sprites.front_default
+                a.src = getThreeLetterId(response.data.id) > 898 ? response.data.sprites.front_default : "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/" + getThreeLetterId(response.data.id)  +".png" 
+                axios
+                  .get(response.data.species.url)
+                  .then(function(resp){
+                    a.color = resp.data.color.name
+                    data.value.push(a)
+                  })
+              })
+          })
+          loading.value = false
+        })
+        .catch(err =>{
+          console.log(err)
+          loading.value = true
+        })
+      }
+
+      onMounted(() =>{
+        fetchData()
+      })
+
+      
+      return{
+        data,
+        loading,
+        error,
+      }
+    },
+    data(){
+      return{
+        search: '',
+        currentPage : 1,
+        loadedImage : 0,
+        displayInfos : false,
+        pokemonToDisplay : {},
+        infoGeneral : true,
+        imageMaxCount : 52
+      }
+    },
+    computed:{
+      pokemonInfoSrc: function(){
+        return this.pokemonToDisplay.id>=650 ? this.pokemonToDisplay.id>=894 ? this.pokemonToDisplay.src : "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/" + this.getThreeLetterId(this.pokemonToDisplay.id)  +".png" : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/"+ this.pokemonToDisplay.id +".svg"
+      }
+    }      
   }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+img{
+  width:55%;
+  height:auto;
+}
+
 h3 {
   margin: 40px 0 0;
 }
@@ -52,7 +312,216 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
-a {
-  color: #42b983;
+
+.loadingScreen{
+  display:flex;
+  align-items:center;
+  justify-content:center;
 }
+
+.pageList{
+  display:flex;
+  flex-direction:row;
+  justify-content:center;
+}
+
+.pageList > li{
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  border-radius:10rem;
+  font-size:1em;
+  width:2.5%;
+  max-height: 5%;
+  border:2px solid black;
+  margin: 3vh 5px 0 5px;
+}
+
+.pageList > li:hover{
+  cursor:pointer;
+  background-color:black;
+  color:white;
+}
+
+#infosView{
+  position: fixed;
+  display:flex;
+  top:15%;
+  left:5%;
+  width:90%;
+  height:80%;
+  background-color:white;
+  opacity:0.95;
+  z-index:3;
+}
+
+#infosView > figure{
+  display:flex;
+  align-items:center;
+  justify-content:center;
+  height:100%;
+}
+
+#infosView > figure > img{
+  max-width:100%;
+  height:auto;
+}
+
+#infosRightSide{
+  display:flex;
+  flex-direction:column;
+  grid-row-gap:0;
+}
+
+#xcross{
+  height:5vh;
+}
+
+#xcross > img{
+  height:100%;
+  width:auto;
+  float:right;
+  padding:1vh;
+}
+
+#xcross > img:hover{
+  cursor:pointer;
+}
+
+#infosNavBar{
+  display:flex;
+  height:10vh;
+  align-items:center;
+  justify-content:center;
+  gap:5vw;
+  font-size:2em;
+}
+
+#infosNavBar > a:hover{
+  cursor:pointer;
+  border-bottom:solid 3px black;
+  transition:0.2s;
+}
+
+#infosNavBar > a{
+  color:gray;
+}
+
+#infosGeneral{
+  height:60vh;
+  display:flex;
+  flex-direction:column;
+  grid-row-gap:0;
+  font-size:2em;
+  align-items:center;
+  justify-content:center;
+}
+
+#infosGeneral > *{
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  width:100%;
+  max-height:10vh;
+}
+
+#infosStats{
+  height:60vh;
+  font-size: 2em;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+  justify-content:center;
+}
+
+.types{
+  border:2px solid black;
+  border-radius:25%;
+  padding: 2vh 2vw;
+}
+
+#displayTypesDetails{
+  display:flex;
+  gap: 0 2vw;
+}
+
+#weightHeightInfo{
+  gap:5%;
+}
+
+/* Small devices (landscape phones, 576px and up)*/
+@media (min-width: 200px) {
+  #infosView{
+    flex-direction:column;
+  }
+  #infosView > figure{
+    height:30%;
+    width:100%;
+    border-bottom:3px solid black;
+    border-right: 0;
+  }
+  #infosRightSide{
+    height:70%;
+    width:100%;
+  }
+  #infosView > figure > img{
+    max-height:100%;
+    width:auto;
+  }
+}
+/* Small devices (landscape phones, 576px and up)*/
+@media (min-width: 567px) {
+  #infosView{
+    flex-direction:column;
+  }
+  #infosView > figure{
+    height:35%;
+    width:100%;
+    border-bottom:3px solid black;
+    border-right: 0;
+  }
+  #infosRightSide{
+    height:65%;
+    width:100%;
+  }
+  #infosView > figure > img{
+    max-height:100%;
+    width:auto;
+  }
+}
+
+/* Medium devices (tablets, 768px and up)*/
+@media (min-width: 768px) {
+  #infosView{
+    flex-direction:row;
+  }
+  #infosView > figure{
+    height:100%;
+    width:50%;
+    border-right:3px solid black;
+    border-bottom: 0;
+  }
+  #infosRightSide{
+    height:100%;
+    width:50%;
+  }
+}
+
+/* Extra large devices (large laptops and desktops, 1200px and up) */
+@media only screen and (min-width: 1200px) {
+  #infosView{
+    flex-direction:row;
+  }   
+  #infosView > figure{
+    height:100%;
+    width:50%;
+    border-right:3px solid black;
+    border-bottom: 0;
+  } 
+  #infosRightSide{
+    height:100%;
+    width:50%;
+  }
+}
+
 </style>
